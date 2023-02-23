9b6e20aad66d2341b30ea659f9b2910be42cde48
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2023-02-23 08:38:34 -0500

    Update NuGet packages

--- .nuget/package.bat
+++ .nuget/package.bat
@@ -28,5 +28,7 @@ copy ..\MaxFactry.General.AspNet.IIS.Mvc4-NF-4.5.2\bin\MaxFactry.General.AspNet.
 c:\install\nuget\nuget.exe pack MaxFactry.General.AspNet.IIS.Mvc4.nuspec -OutputDirectory "packages" -IncludeReferencedProjects -properties Configuration=Release 
 
 copy ..\MaxFactry.General.AspNet.IIS.Mvc5-NF-4.5.2\bin\MaxFactry.General.AspNet.IIS.Mvc5*.dll lib\General.AspNet.IIS.Mvc5\net452\
+copy ..\MaxFactry.General.AspNet.IIS.Mvc5-NF-4.7.2\bin\MaxFactry.General.AspNet.IIS.Mvc5*.dll lib\General.AspNet.IIS.Mvc5\net472\
+copy ..\MaxFactry.General.AspNet.IIS.Mvc5-NF-4.8\bin\MaxFactry.General.AspNet.IIS.Mvc5*.dll lib\General.AspNet.IIS.Mvc5\net48\
 
 c:\install\nuget\nuget.exe pack MaxFactry.General.AspNet.IIS.Mvc5.nuspec -OutputDirectory "packages" -IncludeReferencedProjects -properties Configuration=Release 
