442cdc18a079b46b432334ba033294ae12c937bb
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2025-01-19 21:23:55 -0500

    Update NuGet packages

--- MaxFactry.General.AspNet.IIS.Mvc5-NF-4.5.2/Properties/AssemblyInfo.cs
+++ MaxFactry.General.AspNet.IIS.Mvc5-NF-4.5.2/Properties/AssemblyInfo.cs
@@ -66,6 +66,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.24.31")]
-[assembly: AssemblyFileVersion("2024.11.14.01")]
-[assembly: AssemblyInformationalVersion("1.0.24.31")]
+[assembly: AssemblyVersion("1.0.24.32")]
+[assembly: AssemblyFileVersion("2025.01.19.01")]
+[assembly: AssemblyInformationalVersion("1.0.24.32")]
