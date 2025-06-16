4fc597f06e11709387731859307dd182478e3a96
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2025-06-16 18:24:44 -0400

    Use MaxCacheRepository for caching

--- MaxFactry.General.AspNet.IIS-NF-2.0/System.Web/Caching/MaxOutputCacheProviderverride.cs
+++ MaxFactry.General.AspNet.IIS-NF-2.0/System.Web/Caching/MaxOutputCacheProviderverride.cs
@@ -28,11 +28,14 @@
 #region Change Log
 // <changelog>
 // <change date="9/30/2014" author="Brian A. Lakstins" description="Initial Release">
+// <change date="6/16/2025" author="Brian A. Lakstins" description="Add using this MaxCacheRepository">
 // </changelog>
 #endregion
 
 namespace System.Web.Caching
 {
+    using MaxFactry.Base.DataLayer;
+    using MaxFactry.Base.DataLayer.Library;
     using System;
     using System.Collections.Generic;
 
@@ -43,162 +46,57 @@ namespace System.Web.Caching
     /// </summary>
     public class MaxOutputCacheProviderOverride : OutputCacheProvider
     {
-        private static Dictionary<string, object> _oItemIndex = new Dictionary<string, object>();
-
-        private static Dictionary<string, DateTime> _oExpireIndex = new Dictionary<string, DateTime>();
-
-        private static object _oLock = new object();
+        private string GetKey(string lsKey)
+        {
+            string lsR = this.GetType().ToString() + "/" + MaxDataLibrary.GetApplicationKey() + "/" + lsKey;
+            return lsR;
+        }
 
         public override object Get(string lsKey)
-        {
-            return GetValue(lsKey);
+        {           
+            try
+            {
+                if (null != HttpContext.Current && null != HttpContext.Current.Request && null != HttpContext.Current.Request.QueryString &&
+                    !String.IsNullOrEmpty(HttpContext.Current.Request.QueryString["clearcache"]))
+                {
+                    this.Remove(lsKey);
+                    return null;
+                }
+            }
+            catch { }
+
+            try
+            {
+                if (null != HttpContext.Current && null != HttpContext.Current.Request && null != HttpContext.Current.Request.QueryString &&
+                    !String.IsNullOrEmpty(HttpContext.Current.Request.QueryString["nocache"]))
+                {
+                    return null;
+                }
+            }
+            catch { }
+
+            string lsCacheKey = this.GetKey(lsKey);
+            object loR = MaxCacheRepository.Get(this.GetType(), lsCacheKey, typeof(object));
+            return loR;
         }
 
-        public override object Add(string lsKey, object loItem, DateTime loExpire)
+        public override object Add(string lsKey, object loItem, DateTime ldExpire)
         {
-            return AddValue(lsKey, loItem, loExpire);
+            string lsCacheKey = this.GetKey(lsKey);
+            MaxCacheRepository.Set(this.GetType(), lsCacheKey, loItem, ldExpire);
+            return loItem;
         }
 
-        public override void Set(string lsKey, object loItem, DateTime loExpire)
+        public override void Set(string lsKey, object loItem, DateTime ldExpire)
         {
-            SetValue(lsKey, loItem, loExpire);
+            string lsCacheKey = this.GetKey(lsKey);
+            MaxCacheRepository.Set(this.GetType(), lsCacheKey, loItem, ldExpire);
         }
 
         public override void Remove(string lsKey)
         {
-            RemoveValue(lsKey);
-        }
-
-        public static Dictionary<string, object> GetItemIndex()
-        {
-            return new Dictionary<string, object>(_oItemIndex);
-        }
-
-        public static object GetValue(string lsKey)
-        {
-            try
-            {
-                if (!String.IsNullOrEmpty(HttpContext.Current.Request.QueryString["clearcache"]))
-                {
-                    RemoveValue(lsKey);
-                    return null;
-                }
-            }
-            catch { }
-
-            try
-            {
-                if (!String.IsNullOrEmpty(HttpContext.Current.Request.QueryString["nocache"]))
-                {
-                    return null;
-                }
-            }
-            catch { }
-
-            if (_oItemIndex.ContainsKey(lsKey))
-            {
-                if (_oExpireIndex.ContainsKey(lsKey))
-                {
-                    if (_oExpireIndex[lsKey].ToUniversalTime() < DateTime.Now)
-                    {
-                        RemoveValue(lsKey);
-                        return null;
-                    }
-                }
-
-                return _oItemIndex[lsKey];
-            }
-
-            return null;
-        }
-
-        public static object AddValue(string lsKey, object loItem, DateTime loExpire)
-        {
-            object loCurrent = GetValue(lsKey);
-            if (null != loCurrent)
-            {
-                if (!_oExpireIndex.ContainsKey(lsKey))
-                {
-                    lock (_oLock)
-                    {
-                        if (!_oExpireIndex.ContainsKey(lsKey))
-                        {
-                            _oExpireIndex[lsKey] = loExpire;
-                        }
-                    }
-                }
-
-                return loCurrent;
-            }
-
-            if (!_oItemIndex.ContainsKey(lsKey))
-            {
-                lock (_oLock)
-                {
-                    if (!_oItemIndex.ContainsKey(lsKey))
-                    {
-                        _oItemIndex.Add(lsKey, loItem);
-                    }
-                }
-            }
-
-            if (!_oExpireIndex.ContainsKey(lsKey))
-            {
-                lock (_oLock)
-                {
-                    if (!_oExpireIndex.ContainsKey(lsKey))
-                    {
-                        _oExpireIndex.Add(lsKey, loExpire);
-                    }
-                }
-            }
-
-            return loItem;
-        }
-
-        public static void SetValue(string lsKey, object loItem, DateTime loExpire)
-        {
-            RemoveValue(lsKey);
-            if (!_oItemIndex.ContainsKey(lsKey))
-            {
-                lock (_oLock)
-                {
-                    if (!_oItemIndex.ContainsKey(lsKey))
-                    {
-                        _oItemIndex.Add(lsKey, loItem);
-                    }
-                }
-            }
-
-            if (!_oExpireIndex.ContainsKey(lsKey))
-            {
-                lock (_oLock)
-                {
-                    if (!_oExpireIndex.ContainsKey(lsKey))
-                    {
-                        _oExpireIndex.Add(lsKey, loExpire);
-                    }
-                }
-            }
-        }
-
-        public static void RemoveValue(string lsKey)
-        {
-            if (_oItemIndex.ContainsKey(lsKey))
-            {
-                lock (_oLock)
-                {
-                    if (_oItemIndex.ContainsKey(lsKey))
-                    {
-                        if (_oExpireIndex.ContainsKey(lsKey))
-                        {
-                            _oExpireIndex.Remove(lsKey);
-                        }
-
-                        _oItemIndex.Remove(lsKey);
-                    }
-                }
-            }
+            string lsCacheKey = this.GetKey(lsKey);
+            MaxCacheRepository.Remove(this.GetType(), lsCacheKey);
         }
     }
 #endif
