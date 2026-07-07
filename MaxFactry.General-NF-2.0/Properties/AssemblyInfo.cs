5e4d6a772fb222b9fd04ae6b87c8c87dd69e2abe
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2026-07-07 10:03:01 -0400

    Update references Update Nuget Package for General

--- MaxFactry.General-NF-2.0/Properties/AssemblyInfo.cs
+++ MaxFactry.General-NF-2.0/Properties/AssemblyInfo.cs
@@ -66,6 +66,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.26.22")]
-[assembly: AssemblyFileVersion("2026.06.23.01")]
-[assembly: AssemblyInformationalVersion("1.0.26.22")]
+[assembly: AssemblyVersion("1.0.26.26")]
+[assembly: AssemblyFileVersion("2026.07.07.01")]
+[assembly: AssemblyInformationalVersion("1.0.26.26")]
